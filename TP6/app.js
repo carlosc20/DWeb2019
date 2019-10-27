e42cab12c69790c84ba1de76e73851735ab55c5d
Carlos <cpac20.4@gmail.com>
2019-10-27 16:27:42 +0000

    Completa TP6 menos PUT

--- TP6/app.js
+++ TP6/app.js
@@ -1,11 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -16,11 +14,9 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- TP6/routes/index.js
+++ TP6/routes/index.js
@@ -1,9 +1,85 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+var jsonfile = require('jsonfile')
+var nanoid = require('nanoid')
+
+var arqson = "arq.json"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    jsonfile.readFile(arqson, (err, arq) => {
+        if(err){
+            res.render('error', {message: "Erro na leitura da BD", error: err})
+            return
+        }
+        res.render('index', { lista: arq })
+    })
 });
+
+router.get('/novo', function(req, res, next) {
+    res.render('new-music')
+});
+
+router.post('/', function (req, res) {
+    jsonfile.readFile(arqson, (err, arq) => {
+        if(err){
+            res.render('new-music')
+            return
+        }
+        req.body.id = nanoid()
+        arq.push(req.body)
+        jsonfile.writeFile(arqson, arq, { spaces: 2 },  err => {
+            if(err)
+                res.render('new-music')
+            else
+                res.render('index', { lista: arq })
+        })
+    })
+})
+
+router.delete('/:id', function (req, res) {
+    var id = req.params.id
+    jsonfile.readFile(arqson, (err, arq) => {
+        if(err){
+            res.render('error', {message: "Erro na leitura da BD", error: err})
+            return
+        }
+        var index = arq.findIndex(m => m.id == id)
+        if(index > -1){
+            arq.splice(index,1)
+            jsonfile.writeFile(arqson, arq, { spaces: 2 }, err =>{
+                if(err) console.log(err)
+                else console.log('BD atualizada')
+            })
+        }
+        else {
+            console.log('Erro: não encontrei o elemento a remover...')
+        }
+        res.end('0')
+    })
+})
+
+router.put('/:id', function (req, res) {
+    var id = req.params.id
+    jsonfile.readFile(arqson, (err, arq) => {
+        if(err){
+            res.render('error', {message: "Erro na leitura da BD", error: err})
+            return
+        }
+        var index = arq.findIndex(m => m.id == id)
+        if(index > -1){
+            // arq.push(req.body)
+            jsonfile.writeFile(arqson, arq, { spaces: 2 }, err =>{
+                if(err) console.log(err)
+                else console.log('BD atualizada')
+            })
+        }
+        else {
+            console.log('Erro: não encontrei o elemento a remover...')
+        }
+        res.end('0')
+    })
+})
 
 module.exports = router;
